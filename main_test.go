96461d8009458887324afb0994012a312560b930
GrigoDev <grigodev@yahoo.com>
2025-03-28 17:01:12 +0300

    Add race condition test for main()

--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"sync"
 	"testing"
 )
 
@@ -16,4 +18,18 @@ func TestMaxInt(t *testing.T) {
 
 func TestMain(m *testing.M) {
 	main()
+}
+
+func TestRaceInMain(t *testing.T) {
+	var wg sync.WaitGroup
+	wg.Add(5)
+
+	for i := 0; i < 5; i++ {
+		go func() {
+			fmt.Println(i) // Здесь будет гонка!
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
 }
